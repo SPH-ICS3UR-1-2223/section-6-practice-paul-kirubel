b137bc32c249f7b51ca26daf0aa7a4d36a3ecfbe
PHarryP <paul.harry.theuma-sielinou@stu.ocsb.ca>
2022-10-11 14:32:00 -0400

    Done

--- src/DisplayMultiples/DisplayMultiples.java
+++ src/DisplayMultiples/DisplayMultiples.java
@@ -5,7 +5,15 @@ import java.util.Scanner;
 public class DisplayMultiples {
 
     public static void main(String args[]) {
-       
+       Scanner in = new Scanner(System.in);
+       System.out.println("Enter a Number");  
+       int number = in.nextInt();   
+    
+    for (int counter=1;counter<=12;counter++) {
+    
+    System.out.println(counter*number);
+    
     }
-
+ in.close();
+}
 }
