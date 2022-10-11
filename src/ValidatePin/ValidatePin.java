a809e0ae26ff748a1ed7c91ec1d6dff2251711ef
PHarryP <paul.harry.theuma-sielinou@stu.ocsb.ca>
2022-10-11 13:31:12 -0400

    figure out if it works
    almost finished

--- src/ValidatePin/ValidatePin.java
+++ src/ValidatePin/ValidatePin.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class ValidatePin {
 
     public static void main(String[] args) {
-        
-    }
+   Scanner in = new Scanner(System.in);
+   int pin = 1000;
+   System.out.println("Enter a PIN");
+   int userPin=in.nextInt();
+   while (userPin!=pin) {
+	   System.out.println("Invalid Pin. Try again.");
+   }
+   int userPin=in.nextInt();
+   System.out.println("got it");
+	   }
 }
