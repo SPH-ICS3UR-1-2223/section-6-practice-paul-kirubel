2507b597fdf856138cd969f4baa6f12f7019bf48
PHarryP <paul.harry.theuma-sielinou@stu.ocsb.ca>
2022-10-12 14:40:34 -0400

    triangle

--- src/LoopShape/LoopShape.java
+++ src/LoopShape/LoopShape.java
@@ -4,13 +4,27 @@ public class LoopShape {
     
     static void createRectangle(int width, int height){
         //Draw a Rectangle
-        
-        
+    	//Top line
+   for (int i=1;i<=width;i++) {
+	   System.out.print("#");
+   }
+   System.out.println();
+    System.out.println("#   #");
+    System.out.println("#   #");
+    
+    for (int i=1;i<=width;i++) 
+ 	   System.out.print("#");
+ 	   System.out.println();
     }
     
     static void createTriangle(int leg){
         //Draw an Isosceles Right Triangle
-        
-        
+    
+    	System.out.println();
+   
+    	
+    	for (int i=1;i<leg;i++)  {
+            System.out.print("&");
+        	}
     }
 }
