90f117b1c2a1ff81c7437aba0fb77ee949931c7e
kirubel987 <kirubel.awetahegn@stu.ocsb.ca>
2022-10-13 14:11:49 -0400

    Update LoopShape.java

--- src/LoopShape/LoopShape.java
+++ src/LoopShape/LoopShape.java
@@ -1,16 +1,39 @@
 package LoopShape;
 
 public class LoopShape {
-    
-    static void createRectangle(int width, int height){
+
+	static void createRectangle(int width, int height){
         //Draw a Rectangle
+        for(int i =1;i<width;i++) {
+        	System.out.print("#");
+        	
+        }
         
-        
+    System.out.println();
+    System.out.println("# #");
+    System.out.println("#  #");
+    for (int i=1;i<width;i++)
+    	System.out.print("#");
+    	System.out.println();
+    	
     }
-    
-    static void createTriangle(int leg){
-        //Draw an Isosceles Right Triangle
-        
-        
-    }
+
+	static void createTriangle(int leg) {
+		// Draw an Isosceles Right Triangle
+		for (int i = 1; i < leg; i++) {
+			System.out.print("#");
+			
+			
+		}
+	
+		System.out.println();
+		System.out.println("##");
+		System.out.println("# #");
+		System.out.println("#      #");
+		for (int i = 1; i < leg; i++)
+			System.out.print("#");
+		System.out.println();
+		
+
+	}
 }
